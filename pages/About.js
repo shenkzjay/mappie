f255ab23eed61bc1eb39b1940a2873d3b1c3f934
Chukwuemeka Wisdom <guruwiz2@gmail.com>
2022-01-15 12:12:46 +0100

    Modifications

--- pages/About.js
+++ pages/About.js
@@ -6,9 +6,6 @@ import style from "../styles/About.module.css"
 function About() {
     return (
         <div>
-            <Head>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-            </Head>
             <div className={style.bigwrap}>
                 <Layout>
                     <div className={style.title}><h1>About us</h1></div>
--- pages/Home.js
+++ pages/Home.js
@@ -1,28 +1,93 @@
+//react
+import { useState, useMemo } from 'react'
+// style
 import style from "../styles/Home.module.css"
+// layout
 import Layout from "../components/Layout"
+
+// link
 import Link from "next/link";
+
+// Head
 import Head from "next/head"
+
+// dynamic
 import dynamic from "next/dynamic"
 
+// metaData
+import metaDatas from '../data/metadata.json'
 
+// categories
+import categories from '../data/categories.json'
+
+// MapDisplay
+// import MapDisplay from '../components/MapDisplay';
 
 
 
 //Home function
 function Home() {
-    const MapDisplay = dynamic(() => import("../components/MapDisplay"), {
+
+    // dynamically load map component
+    const MapDisplay = useMemo(() => dynamic(() => import("../components/MapDisplay"), {
         loading: () => <p>Loading...</p>,
-        ssr: false
-      });
-      
+        ssr: false,
+    }), []);
+
+
+
+    // init filteredMetaData state
+    const [filteredMetaData, setFilteredMetaData] = useState([])
+
+
+    // init sortMetaData
+    const sortMetaData = (categoryId) => {
+
+        // filter metadata by categoryId
+        const categoryMetaData = metaDatas.filter((data) => data.categoryId === categoryId)
+
+
+        // reduce categoryMetada into groups 
+        const _filteredMetaData = Object.values(categoryMetaData.reduce((prev, { country, latitude, longitude, categoryName, ...props }) => {
+
+            // check if country is not a key in previous object
+            if (!prev[country]) {
+
+                // init newObject
+                const newObject = { country, latitude, longitude, categoryName, _meta: [props], _metaTotal: [props].length }
+
+                // use country as key in previous object and pass in newObject
+                prev[country] = newObject
+
+            } else {
+
+                // if country is a key in previous object, if yes, then push all data (props) into _meta array
+                prev[country]._meta.push(props)
+
+                // then update _metaTotal by getting the length of previous _meta
+                prev[country]._metaTotal = prev[country]._meta.length
+
+            }
+
+            // return prev object, to iterate again
+            return prev
+        }, {}))
+
+        console.log(_filteredMetaData)
+
+        // update filteredMeta state
+        setFilteredMetaData(_filteredMetaData)
+
+    }
+
+
+
     return (
         <div>
-            <Head>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-            </Head>
+
             <Layout>
-            {/* overall wrapper */}
-                <div className={style.wrap}>    
+                {/* overall wrapper */}
+                {/* <div className={style.wrap}>
                     <div className={style.leftbar}>
                         <div>
                             <div className={style.leftbarcard_top}>
@@ -31,83 +96,88 @@ function Home() {
                                     and one of Africa&apos;s most influential Blockchain
                                     and Crypto media and consultancy firm,
                                     based in Nairobi, Kenya. We are the go-to entry
-                                     point of all matters blockchain in Africa for
+                                    point of all matters blockchain in Africa for
                                     both our audiences and clients.
-                            </p>
+                                </p>
                             </div>
                         </div>
 
 
                         <div>
-                            {/* sidebar information stats display */}
-                            <div className={style.leftcard_bottom}>
+                           
+                            <div className={style.leftcard_bottom} style={{ paddingTop: 10 }}>
+                                <label>Crypto Stats</label>
                                 <br />
-                                <label>Crypto Stats</label><Link href="/">
-                                <div className={style.leftcard_cards}>
-                                    <div>Blockchain Association</div>
-                                    <div className={style.spotter}>
-                                    {/* <div className={style.leftcardbox}>40</div> */}
-                                    <div><img src="/arrow.svg" alt="decking" ></img></div>
+                                {categories.map((category, index) => {
+                                    return <div key={index} className={style.leftcard_cards} style={{ cursor: "pointer" }} onClick={() => sortMetaData(category._id)}>
+                                        <div >{category.title}</div>
+                                        <div className={style.spotter}>
+                                            
+                                            <div><img src="/arrow.svg" alt="decking"></img></div>
+                                        </div>
                                     </div>
-                                    </div></Link>
-
-                                    <Link href="/">
-                                    <div className={style.leftcard_cards}>
-                                    <div>Projects</div>
-                                    <div className={style.spotter}>
-                                    {/* <div className={style.leftcardbox}>12</div> */}
-                                    <div><img src="/arrow.svg" alt="decking" ></img></div>
-                                    </div>
-                                    </div></Link>
-                                    
-                                    <Link href="/">
-                                    <div className={style.leftcard_cards}>
-                                    <div>Defi wallet stats</div>
-                                    <div className={style.spotter}>
-                                    {/* <div className={style.leftcardbox}>400,324</div> */}
-                                    <div><img src="/arrow.svg" alt="decking"></img></div>
-                                    </div>
-                                    </div></Link>
-                                    
-                                    <Link href="/">
-                                    <div className={style.leftcard_cards}>
-                                    <div>P2P Stats</div>
-                                    <div className={style.spotter}>
-                                    {/* <div className={style.leftcardbox}>230,234.34</div> */}
-                                    <div><img src="/arrow.svg" alt="decking" ></img></div>
-                                    </div>
-                                    </div></Link>
-
-                                    <Link href="/">
-                                    <div className={style.leftcard_cards}>
-                                    <div>Crypto events</div>
-                                    <div className={style.spotter}>
-                                    {/* <div className={style.leftcardbox}>2</div> */}
-                                    <div><img src="/arrow.svg" alt="decking" ></img></div>
-                                    </div>
-                                    </div></Link>
-
-                                    <Link href="/">
-                                    <div className={style.leftcard_cards}>
-                                    <div>Country notable crypto report</div>
-                                    <div className={style.spotter}>
-                                    {/* <div className={style.leftcardbox}>9</div> */}
-                                    <div><img src="/arrow.svg" alt="decking" ></img></div>
-                                    </div>
-                                    </div></Link>
-                               
-                               
+                                })}
 
                             </div>
                         </div>
                     </div>
-                    {/* map area */}
+                  
                     <div className={style.rightbar}>
                         <div>
-                           <MapDisplay />
+
+                            <MapDisplay metaDataList={filteredMetaData} />
+
                         </div>
                     </div>
-                </div>
+                </div> */}
+
+                <main>
+                    <section className="section section-header text-dark pb-md-5 pt-5">
+                        <div className="container pt-4 pb-5">
+                            <div className="row justify-content-center">
+                                <div className="col-12 col-md-10 text-center ">
+                                    <h1 className="display-3 font-weight-bolder mb-4">
+                                        Best Crypto &amp; Blockchain Media In Africa
+                                    </h1>
+                                    <p className="lead mb-4 mb-lg-5 lh-3">We stand as a Blockchain and Crypto authority,
+                                        and one of Africa&apos;s most influential Blockchain
+                                        and Crypto media and consultancy firm,
+                                        based in Nairobi, Kenya. We are the go-to entry
+                                        point of all matters blockchain in Africa for
+                                        both our audiences and clients.</p>
+
+                                </div>
+                            </div>
+                            <div className="row mt-4">
+                                <div className="col-12 col-md-5 justify-content-md-center pe-1 ps-1 ps-md-1">
+                                    <div>
+
+                                        <div className={style.leftcard_bottom} style={{ paddingTop: 10 }}>
+                                            <label className="text-dark">Crypto Stats</label>
+                                            <br />
+                                            {categories.map((category, index) => {
+                                                return <div key={index} className={style.leftcard_cards} style={{ cursor: "pointer" }} onClick={() => sortMetaData(category._id)}>
+                                                    <div >{category.title}</div>
+                                                    <div className={style.spotter}>
+
+                                                        <div><img src="/arrow.svg" alt="decking"></img></div>
+                                                    </div>
+                                                </div>
+                                            })}
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-12 col-md-7 ps-1 ps-md-0 pe-1 mt-3 mt-md-0">
+
+                                    <MapDisplay metaDataList={filteredMetaData} />
+
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </main>
             </Layout>
         </div>
     )
